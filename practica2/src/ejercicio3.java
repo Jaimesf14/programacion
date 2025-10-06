b508d14d1bcd7da815181f1f433536d3da47754d
Practica 1 <jaimesf3r@gmail.com>
2025-10-06 14:18:41 +0200

    ejercicio3

---
+++ practica2/src/ejercicio3.java
@@ -0,0 +1,13 @@
+public class ejercicio3 {
+    //Crea un array que contenga 5 números. Realiza un programa que
+    // te muestre por pantalla SOLO el más pequeño de ellos.
+    public static void main(String[] args){
+        int[] n = {10, 2, 7, 5, 3};
+        for (
+                if (n[0] < n[1]) {
+                    n[1] > n[0];
+                } if (n)
+        }
+    }
+}
+
