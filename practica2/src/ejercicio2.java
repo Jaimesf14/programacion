b2980f322069f98deaaf8944a2c7df788d5f4d06
Practica 1 <jaimesf3r@gmail.com>
2025-10-06 13:40:43 +0200

    ejercicio2

---
+++ practica2/src/ejercicio2.java
@@ -0,0 +1,10 @@
+public class ejercicio2 {
+    //Muestra por pantalla todos los elementos de un array de números enteros separados por un espacio.
+    public static void main(String[] args){
+        int[] n = {10, 20, 30, 40, 50, 60, 70, 80, 90, };
+        for (int i = 0; i< n.length; i++) {
+            System.out.print(n[i] + " ");
+        }
+
+    }
+}
