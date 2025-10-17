f2e9d4debe648732c0061a1866012b50090c9890
Practica 1 <jaimesf3r@gmail.com>
2025-10-17 09:18:46 +0200

    Mostrar el numero más pequeño

--- practica2/src/ejercicio3.java
+++ practica2/src/ejercicio3.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class ejercicio3 {
     public static void main(String[] args) {
-        //Crea un array que contenga 5 números. Realiza un programa que te muestre por pantalla SOLO el más pequeño de ellos.
+        //Crea un array que contenga 5 números. Realiza un programa que te muestre por pantalla SOLO el más
+        //pequeño de ellos.
 
         int[] numero = {10, 25, 5, 32, 12};
         int menor =  numero[0];
