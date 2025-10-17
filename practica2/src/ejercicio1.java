22bb0214d4c7a9e3485f88644ae17dc1000a0dcc
Practica 1 <jaimesf3r@gmail.com>
2025-10-17 09:25:37 +0200

    Crea un array de 10 posiciones de números con valores pedidos por teclado. Muestra por consola el índice y el valor al que corresponde.

--- practica2/src/ejercicio1.java
+++ practica2/src/ejercicio1.java
@@ -8,12 +8,12 @@ public class ejercicio1 {
         //Crea un array de 10 posiciones de números con valores pedidos por teclado.
         Scanner sc = new Scanner(System.in);
         System.out.println("introduce otro número:");
+
         int[] arrayInt = new int[10];
         for (int i = 0; i< arrayInt.length; i++) {
-
             arrayInt[i] = sc.nextInt();
         }
-            //Muestra por consola el índice y el valor al que corresponde.
+
         for (int i = 0; i < arrayInt.length; i++) {
             System.out.println("indice " + i + " --> " + arrayInt[i]);
         }
