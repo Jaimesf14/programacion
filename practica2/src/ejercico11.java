d2c8224f00b6af5b863c1730460395c4381db4f6
Jaime <jaimesf3r@gmail.com>
2025-10-23 20:18:55 +0200

    Escribe un programa java que invierta el orden de los valores de un array. Por invertir el orden de los valores de un array, me refiero que el último pasa a ser el primero, el penúltimo el segundo y así sucesivamente. PRUEBA CON UN ARRAY DE TAMAÑO 6.

--- practica2/src/ejercico11.java
+++ practica2/src/ejercico11.java
@@ -3,6 +3,17 @@ public class ejercico11 {
         //Escribe un programa java que invierta el orden de los valores de un array.
         // Por invertir el orden de los valores de un array, me refiero que el último pasa a ser el primero,
         // el penúltimo el segundo y así sucesivamente. PRUEBA CON UN ARRAY DE TAMAÑO 6.
-
+        int[] array = {1,2,3,4,5,6};
+        int[] invertido = new int[array.length];
+        System.out.println("Array original: ");
+        for (int i = 0; i< array.length; i++){
+        System.out.print(array[i] + " ");
+        }
+        System.out.println(" ");
+        System.out.println("Array invertido: ");
+        for (int i = 0; i< array.length; i++){
+            invertido[i] = array[array.length - 1 - i];
+            System.out.print(invertido[i] + " ");
+        }
     }
 }
