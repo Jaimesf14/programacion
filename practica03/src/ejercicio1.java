97e25b5fe5887eaba684506f511ce33804d00e11
Practica 1 <jaimesf3r@gmail.com>
2025-11-03 09:25:06 +0100

    Ejercicio1

--- practica03/src/ejercicio1.java
+++ practica03/src/ejercicio1.java
@@ -5,17 +5,20 @@ public class ejercicio1 {
               {75,1.5,1.5,1.5,0,1.5},
               {1.5,1.5,-2,9,1.5,11}
       };
-      System.out.println("Array num Columna 1 Columna 2 Columna 3 Columna 4 Columna 5 Columna 6");
+        System.out.println("------------------------------------------------------------------------------");
+      System.out.println("|Array num |Columna 0 |Columna 1 |Columna 2 |Columna 3 |Columna 4 |Columna 5 |");
         for (int i = 0; i < numeros.length; i++) {
-            System.out.print("Fila " + i + "   |");
+            System.out.println("|----------+----------+----------+----------+----------+----------+----------|");
+            System.out.print("|Fila " + i + "    |");
             for (int j = 0; j < numeros[i].length; j++) {
                 if (numeros[i][j] == 1.5){
-                    System.out.print("         |");
+                    System.out.print("          |");
                 } else {
-                    System.out.printf("%4%-4.0f |", numeros[i][j]);
+                    System.out.printf("%9.0f |", numeros[i][j]);
                 }
             }
             System.out.println(" ");
         }
+        System.out.println("------------------------------------------------------------------------------");
     }
 }
