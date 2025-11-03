64e2d9c1fdb9b789151995fc139155dcf337b8b2
Practica 1 <jaimesf3r@gmail.com>
2025-11-03 10:10:14 +0100

    Ejercicio1

--- practica03/src/ejercicio1.java
+++ practica03/src/ejercicio1.java
@@ -4,7 +4,7 @@ public class ejercicio1 {
         // la siguiente tabla. Muestra el contenido de todos los elementos del array dispuestos en forma de tabla
         // como se muestra en la figura.
         //Definimos el array de tipo double, y ponermos el numero 1.5 para sustituirlo por un espacio.
-      double numeros [][] = {
+      double[][] numeros  = {
               {0,30,2,1.5,1.5,5},
               {75,1.5,1.5,1.5,0,1.5},
               {1.5,1.5,-2,9,1.5,11}
