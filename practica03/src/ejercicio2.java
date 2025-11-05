c1edb10747eca463236a64e5326fc33c1a617045
Practica 1 <jaimesf3r@gmail.com>
2025-11-05 14:24:17 +0100

    Ejercicio1

--- practica03/src/ejercicio2.java
+++ practica03/src/ejercicio2.java
@@ -1,3 +1,4 @@
+import java.awt.datatransfer.SystemFlavorMap;
 import java.util.Scanner;
 
 public class ejercicio2 {
@@ -5,7 +6,7 @@ public class ejercicio2 {
     // por 5 columnas. El programa mostrará las sumas parciales de filas y en las columnas el mayor número de la columna.
     // La suma total debe aparecer en la esquina inferior derecha.
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         /*System.out.println("introduce 20 número:");
 
@@ -17,32 +18,47 @@ public class ejercicio2 {
 
             }
         }*/
-        int[][] numeros  = {
+        int[][] numeros = {
                 {11, 12, 13, 14, 15},
                 {16, 17, 18, 19, 10},
                 {21, 22, 23, 14, 15},
                 {16, 17, 18, 19, 20}
         };
+        int sumatotal = 0;
 
 
-        System.out.println("-------------------------------------------------------------------");
+        System.out.println("----------------------------------------------------------------------------------");
         for (int i = 0; i < numeros.length; i++) {
             int sumafila = 0;
-            sumafila = numeros[i][0] + numeros[i][1] + numeros[i][2] + numeros[i][3] + numeros[i][4];
-
             for (int j = 0; j < numeros[i].length; j++) {
-                int sumacolumna = 0;
-                    System.out.printf("| %9d",  numeros[i][j]);
-                sumacolumna = numeros[0][j] + numeros[1][j] + numeros[2][j] + numeros[3][j];
+
+                sumafila += numeros[i][j];
+                System.out.printf("| %-11d", numeros[i][j]);
+
 
             }
-            System.out.print("|Fila " + i + "  " + sumafila + "|");
+            System.out.print("|Fila " + i + ": " + sumafila + "     |");
             System.out.println(" ");
-            System.out.println("|----------+----------+----------+----------+----------+----------|");
+            System.out.println("|------------+------------+------------+------------+------------+---------------|");
+            sumatotal += sumafila;
+
         }
 
-        System.out.println("|Array num |Columna 0 |Columna 1 |Columna 2 |Columna 3 |Columna 4 |");
-        System.out.println("-------------------------------------------------------------------");
+        for (int j = 0; j < numeros[0].length; j++) {
+            int mayorcolumna = numeros[0][j];
+
+            for (int i = 0; i < numeros.length; i++) {
+                if (numeros[i][j] > mayorcolumna) {
+                    mayorcolumna = numeros[i][j];
+                }
+            }
+
+            System.out.printf("|Columna" + j +": "+ mayorcolumna);
+
+
+        } System.out.println("|Suma total: "+ sumatotal+"|");
+        System.out.print("----------------------------------------------------------------------------------");
     }
 }
+
 
