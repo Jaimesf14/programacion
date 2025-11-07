65149ac0018976694c897d7ff3493c0864554a58
Practica 1 <jaimesf3r@gmail.com>
2025-11-07 11:47:42 +0100

    Ejercicio3

--- practica03/src/ejercicio3.java
+++ practica03/src/ejercicio3.java
@@ -2,26 +2,20 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class ejercicio3 {
+    //Modifica el programa anterior de tal forma que los números que se introducen en el array se generen de forma aleatoria (valores entre 1 y 1999).
     public static void main(String[] args) {
+        //Generamos la variable numaleatorio, con numeros aleatorios.
         Random numaleatorio = new Random();
-
+        //Definimos el array
         int[][] numeros = new int[4][5];
-
+        //recorremos el bucle y definimos que queremos numeros aleatorios entre 1 y 2000
         for (int i = 0; i < numeros.length; i++) {
             for (int j = 0; j < numeros[i].length; j++) {
                 numeros[i][j] = numaleatorio.nextInt(1999) + 1;
 
             }
         }
-        /*
-        int[][] numeros = {
-                {11, 12, 13, 14, 15},
-                {16, 17, 18, 19, 10},
-                {21, 22, 23, 14, 15},
-                {16, 17, 18, 19, 20}
-        };
-        */
-
+        //El resto es igual que el ejercicio anterior.
         int sumatotal = 0;
         System.out.println("----------------------------------------------------------------------------------------------");
         for (int i = 0; i < numeros.length; i++) {
