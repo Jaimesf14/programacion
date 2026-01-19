21c2ccf36a32af5f6a6e99906e4df4f38589ea4a
Practica 1 <jaimesf3r@gmail.com>
2026-01-19 10:31:09 +0100

    Modelo

--- practica05/src/com/juego/modelo/Personaje.java
+++ practica05/src/com/juego/modelo/Personaje.java
@@ -1,10 +1,106 @@
 package com.juego.modelo;
 
 public class Personaje {
-    //atributo de los personajes
+    //atributos
+    private String nombre;
+    private int fuerza;
+    private int destreza;
+    private int inteligencia;
+    private int vidaBase;
+    private String especialidad;
+    private int vidaMax;
+
+
+    //constructor
+    public Personaje(String nombre, int fuerza, int destreza, int inteligencia, int vidaBase,
+                     String especialidad, int vidaMax){
+        this.nombre = nombre;
+        this.fuerza = fuerza;
+        this.destreza = destreza;
+        this.inteligencia = inteligencia;
+        this.vidaBase = vidaBase;
+        this.especialidad = especialidad;
+        this.vidaMax = vidaMax;
+    }
+
+
+    //get y set
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public int getFuerza() {
+        return fuerza;
+    }
+
+
+    public void setFuerza(int fuerza) {
+        this.fuerza = fuerza;
+    }
+
+
+    public int getDestreza() {
+        return destreza;
+    }
+
+
+    public void setDestreza(int destreza) {
+        this.destreza = destreza;
+    }
+
+
+    public int getInteligencia() {
+        return inteligencia;
+    }
+
+
+    public void setInteligencia(int inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+
+
+    public int getVidaBase() {
+        return vidaBase;
+    }
+
+
+    public void setVidaBase(int vidaBase) {
+        this.vidaBase = vidaBase;
+    }
+
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+
+    public int getVidaMax() {
+        return vidaMax;
+    }
+
+
+    public void setVidaMax(int vidaMax) {
+        this.vidaMax = vidaMax;
+    }
+}
+
+    /*//atributo de los personajes
     private String nombre;
     private int fuerza;
     private int inteligencia;
     private int destreza;
     private int vida;
-}
+}*/
---
+++ practica05/src/com/juego/presentacion/Presentacion.java
@@ -0,0 +1,16 @@
+package com.juego.presentacion;
+
+public class Presentacion {
+    /*
+    1. Jugar
+    new Combate()
+    dentro de combate:
+    1.1 elegir personaje (ArrayList en gestor personajes)
+    1.2 turno 1 (bucle)
+    p1 -> vida, habilidades con usos
+    p2 -> vida, habilidades con usos
+    finaliazcion de turno (ha muerto alguno de los dos?)
+    2. Crear personaje
+    3. Salir
+     */
+}
