48cae1281594943442167d41e91095e329d1f0ca
jaimesf14 <jaimesf3r@gmail.com>
2026-05-22 14:14:37 +0200

    modificaciones combate

--- practica8/src/rpg/logic/MotorCombate.java
+++ practica8/src/rpg/logic/MotorCombate.java
@@ -60,12 +60,12 @@ public class MotorCombate {
 
         System.out.println("Selecciona la id del segundo personaje que quieres que compita: ");
         for (Personajes p : personajes){
-            System.out.println("-ID: " + p.getId() + " - Nombre: " + p.getNombre());
+            System.out.println("- ID: " + p.getId() + " - Nombre: " + p.getNombre());
         }
         int id2 = s.nextInt();
         s.nextLine();
 
-        Personajes personaje2 = personajesDAO.buscarPersonajesPorId(id1);
+        Personajes personaje2 = personajesDAO.buscarPersonajesPorId(id2);
         if (personaje2 ==  null){
             System.out.println("Id del personaje no valido");
             return;
@@ -79,7 +79,82 @@ public class MotorCombate {
             System.out.println("| Personaje 1 | ID: " + personaje1.getId() + " - Nombre: " + personaje1.getNombre() +" - Vida: "+ personaje1.getVida_actual());
             System.out.println("| Personaje 2 | ID: " + personaje2.getId() + " - Nombre: " + personaje2.getNombre() +" - Vida: "+ personaje2.getVida_actual());
 
+            Personajes atacante;
+            Personajes defensor;
+
+            if (turno == 1){
+                atacante = personaje1;
+                defensor = personaje2;
+            } else {
+                atacante = personaje2;
+                defensor = personaje1;
+            }
+
+            System.out.println("| Turno de " +atacante.getNombre()+ " de atacar.");
+            System.out.println("| Elige el ide de la habilidad que quieres que use: ");
+            for (Map.Entry<Habilidades, Boolean> h:atacante.getHabilidades_equipadas().entrySet()){
+                System.out.println("- ID: " + h.getKey().getId() + " - Nombre: " + h.getKey().getNombre() + " - Daño: " + h.getKey().getDaño_base() + " - Usos: " + h.getKey().getUsos_maximos());
+            }
+            int idHabilidad = s.nextInt();
+            s.nextLine();
+
+            Habilidades habilidadSeleccionada = habilidadesDAO.buscarHabilidadesPorId(idHabilidad);
+            if (habilidadSeleccionada == null){
+                System.out.println("Id de la habilidad elegida no valida.");
+                return;
+            }
+
+            if (habilidadSeleccionada.getUsos_maximos()>0){
+                int danioTotal = habilidadSeleccionada.getDaño_base();
+                defensor.setVida_actual(defensor.getVida_actual()-danioTotal);
+                habilidadSeleccionada.setUsos_maximos(habilidadSeleccionada.getUsos_maximos()-1);
+                System.out.println("| El personaje " + atacante.getNombre() + " ha usado la habilidad " + habilidadSeleccionada.getNombre());
+
+
+                String sql = "UPDATE personajes SET vida_actual = ? WHERE id = ?";
+                try(Connection conn = getConnection();
+                    PreparedStatement pstmt = conn.prepareStatement(sql)){
+                    pstmt.setInt(1, defensor.getVida_actual());
+                    pstmt.setInt(2, defensor.getId());
+                    pstmt.executeUpdate();
+
+                } catch (SQLException e) {
+                    LoggerCustom.info("[" + LocalDateTime.now() + "] ERROR: Error al  restarle vida al personaje " + defensor.getNombre() + " - " +e.getMessage());
+                    e.printStackTrace();
+                }
+
+                String sql2 = "UPDATE habilidades SET usos_maximos = ? WHERE id = ?";
+                try(Connection conn = getConnection();
+                    PreparedStatement pstmt = conn.prepareStatement(sql2)){
+                    pstmt.setInt(1, habilidadSeleccionada.getUsos_maximos());
+                    pstmt.setInt(2, habilidadSeleccionada.getId());
+                    pstmt.executeUpdate();
+
+                } catch (SQLException e) {
+                    LoggerCustom.info("[" + LocalDateTime.now() + "] ERROR: Error al restarle un uso a la habilidad " + habilidadSeleccionada.getNombre() + " - " +e.getMessage());
+                    e.printStackTrace();
+                }
+
+            } else {
+                System.out.println("Pierde turno");
+            }
+
+            //Cambiar turno
+            if (turno == 1) {
+                turno = 2;
+            }
+            else{
+                turno = 1;
+            }
+            cont++;
         }
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
+        if (personaje1.getVida_actual()>0){
+            System.out.println("| El ganador es: " + personaje1.getNombre());
+        } else{
+            System.out.println("| El ganador es: " + personaje2.getNombre());
+        }
+        System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------");
     }
 
 }
